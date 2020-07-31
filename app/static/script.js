49c5b9a2b52dcf8a5013b5b2fdf26047934c5959
ADI <38807207+DaarkStar@users.noreply.github.com>
2020-07-31 05:48:29 +0530

    VC Interface, Docs In VC, Terms Nad Conditions

--- app/static/script.js
+++ app/static/script.js
@@ -1,18 +1,18 @@
 
 var firebaseConfig = {
-    apiKey: "AIzaSyCNtuLp9_8dTKGHGnYTQDvtc4sjdG6Al8Q",
-    authDomain: "pdochealth.firebaseapp.com",
-    databaseURL: "https://pdochealth.firebaseio.com",
-    projectId: "pdochealth",
-    storageBucket: "pdochealth.appspot.com",
-    messagingSenderId: "781169590792",
-    appId: "1:781169590792:web:247b3e1249dac8e0e8c7bd",
-    measurementId: "G-56VD6RED0L"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+  apiKey: "AIzaSyCNtuLp9_8dTKGHGnYTQDvtc4sjdG6Al8Q",
+  authDomain: "pdochealth.firebaseapp.com",
+  databaseURL: "https://pdochealth.firebaseio.com",
+  projectId: "pdochealth",
+  storageBucket: "pdochealth.appspot.com",
+  messagingSenderId: "781169590792",
+  appId: "1:781169590792:web:247b3e1249dac8e0e8c7bd",
+  measurementId: "G-56VD6RED0L"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 if (!location.hash) {
-  location.hash = location.hash = window.location.pathname.split("/")[window.location.pathname.split("/").length-1];
+location.hash = location.hash = window.location.pathname.split("/")[window.location.pathname.split("/").length-1];
 }
 const roomHash = window.location.pathname.split("/")[window.location.pathname.split("/").length-1];
 
@@ -21,9 +21,9 @@ const drone = new ScaleDrone('2xmbUiTsqTzukyf7');
 // Room name needs to be prefixed with 'observable-'
 const roomName = 'observable-' + roomHash;
 const configuration = {
-  iceServers: [{
-    urls: 'stun:stun.l.google.com:19302'
-  }]
+iceServers: [{
+  urls: 'stun:stun.l.google.com:19302'
+}]
 };
 let room;
 let pc;
@@ -31,230 +31,279 @@ let pc;
 
 function onSuccess() {};
 function onError(error) {
-  alert(error);
+console.error(error);
 };
 
 var database = firebase.database();
 database.ref('meeting/' + roomHash).set({"summary":"Diagnosis summary"});
 ref = database.ref("meeting/"+roomHash);
 ref.on("child_changed", function(snapshot) {
-  console.log(snapshot.val());
+console.log(snapshot.val());
 }, function (errorObject) {
-  console.log("The read failed: " + errorObject.code);
+console.log("The read failed: " + errorObject.code);
 });
 
 
 ref = database.ref("meeting/"+roomHash+"/");
 $("#summary").keyup(function(){
-    summary = $(this).val();
-    ref.update({"summary":summary})
+  summary = $(this).val();
+  ref.update({"summary":summary})
 });
 
 
 
 drone.on('open', error => {
+if (error) {
+  return console.error(error);
+}
+room = drone.subscribe(roomName);
+room.on('open', error => {
   if (error) {
-    return console.error(error);
+    onError(error);
   }
-  room = drone.subscribe(roomName);
-  room.on('open', error => {
-    if (error) {
-      onError(error);
-    }
-  });
-  // We're connected to the room and received an array of 'members'
-  // connected to the room (including us). Signaling server is ready.
-  room.on('members', members => {
-    console.log('MEMBERS', members);
-    // If we are the second user to connect to the room we will be creating the offer
-    const isOfferer = members.length === 2;
-    startWebRTC(isOfferer);
-  });
+});
+// We're connected to the room and received an array of 'members'
+// connected to the room (including us). Signaling server is ready.
+room.on('members', members => {
+  console.log('MEMBERS', members);
+  // If we are the second user to connect to the room we will be creating the offer
+  const isOfferer = members.length === 2;
+  startWebRTC(isOfferer);
+});
 });
 
 // Send signaling data via Scaledrone
 function sendMessage(message) {
-  drone.publish({
-    room: roomName,
-    message
-  });
+drone.publish({
+  room: roomName,
+  message
+});
+}
+var temp_stream;
+function startWebRTC(isOfferer) {
+pc = new RTCPeerConnection(configuration);
+
+// 'onicecandidate' notifies us whenever an ICE agent needs to deliver a
+// message to the other peer through the signaling server
+pc.onicecandidate = event => {
+  if (event.candidate) {
+    sendMessage({'candidate': event.candidate});
+  }
+};
+
+// If user is offerer let the 'negotiationneeded' event create the offer
+if (isOfferer) {
+  pc.onnegotiationneeded = () => {
+    pc.createOffer().then(localDescCreated).catch(onError);
+  }
 }
 
-function startWebRTC(isOfferer) {
-  pc = new RTCPeerConnection(configuration);
+// When a remote stream arrives display it in the #remoteVideo element
+pc.onaddstream = event => {
+  remoteVideo.srcObject = event.stream;
+};
 
-  // 'onicecandidate' notifies us whenever an ICE agent needs to deliver a
-  // message to the other peer through the signaling server
-  pc.onicecandidate = event => {
-    if (event.candidate) {
-      sendMessage({'candidate': event.candidate});
-    }
-  };
+navigator.mediaDevices.getUserMedia({
+  audio: true,
+  video: true
+}).then(stream => {
+  // Display your local video in #localVideo element
+  localVideo.srcObject = stream;
+  // Add your stream to be sent to the conneting peer
+  pc.addStream(stream);
+  temp_stream = stream;
+}, onError);
 
-  // If user is offerer let the 'negotiationneeded' event create the offer
-  if (isOfferer) {
-    pc.onnegotiationneeded = () => {
-      pc.createOffer().then(localDescCreated).catch(onError);
-    }
+// Listen to signaling data from Scaledrone
+room.on('data', (message, client) => {
+  // Message was sent by us
+  if (client.id === drone.clientId) {
+    return;
   }
 
-  // When a remote stream arrives display it in the #remoteVideo element
-  pc.ontrack = event => {
-    remoteVideo.srcObject = event.stream;
-  };
+  if (message.sdp) {
+    // This is called after receiving an offer or answer from another peer
+    pc.setRemoteDescription(new RTCSessionDescription(message.sdp), () => {
+      // When receiving an offer lets answer it
+      if (pc.remoteDescription.type === 'offer') {
+        pc.createAnswer().then(localDescCreated).catch(onError);
+      }
+    }, onError);
+  } else if (message.candidate) {
+    // Add the new ICE candidate to our connections remote description
+    pc.addIceCandidate(
+      new RTCIceCandidate(message.candidate), onSuccess, onError
+    );
+  }
+});
+}
+let mic_switch = true;
+let video_switch = true;
 
-  navigator.mediaDevices.getUserMedia({
-    audio: true,
-    video: true
-  }).then(stream => {
-    // Display your local video in #localVideo element
-    localVideo.srcObject = stream;
-    // Add your stream to be sent to the conneting peer
-    pc.addStream(stream);
-  }, onError);
+function toggleVideo() {
+console.log("Vdo togggggggggggggggggg");
+if (temp_stream != null && temp_stream.getVideoTracks().length > 0) {
+  video_switch = !video_switch;
+  temp_stream.getVideoTracks()[0].enabled = video_switch;
+}
+  if(video_switch){
+      $("#pause-icon").removeClass("fa-play");
+      $("#pause-icon").addClass("fa-pause");
 
-  // Listen to signaling data from Scaledrone
-  room.on('data', (message, client) => {
-    // Message was sent by us
-    if (client.id === drone.clientId) {
-      return;
-    }
+  }
+  else{
+      $("#pause-icon").removeClass("fa-pause");
+      $("#pause-icon").addClass("fa-play");
+  }
+}
 
-    if (message.sdp) {
-      // This is called after receiving an offer or answer from another peer
-      pc.setRemoteDescription(new RTCSessionDescription(message.sdp), () => {
-        // When receiving an offer lets answer it
-        if (pc.remoteDescription.type === 'offer') {
-          pc.createAnswer().then(localDescCreated).catch(onError);
-        }
-      }, onError);
-    } else if (message.candidate) {
-      // Add the new ICE candidate to our connections remote description
-      pc.addIceCandidate(
-        new RTCIceCandidate(message.candidate), onSuccess, onError
-      );
-    }
-  });
+function toggleMic() {
+console.log("mic togggggggggggggggggg");
+if (temp_stream != null && temp_stream.getAudioTracks().length > 0) {
+  mic_switch = !mic_switch;
+
+  temp_stream.getAudioTracks()[0].enabled = mic_switch;
+}
+if(mic_switch){
+      $("#mute-icon").removeClass("fa-microphone");
+      $("#mute-icon").addClass("fa-microphone-slash");
+
+  }
+  else{
+      $("#mute-icon").removeClass("fa-microphone-slash");
+      $("#mute-icon").addClass("fa-microphone");
+  }
 }
 
 function localDescCreated(desc) {
-  pc.setLocalDescription(
-    desc,
-    () => sendMessage({'sdp': pc.localDescription}),
-    onError
-  );
+pc.setLocalDescription(
+  desc,
+  () => sendMessage({'sdp': pc.localDescription}),
+  onError
+);
 }
 
 $("#end").click(function(){
-  if (confirm("End Meeting?")) {
-    $.ajax({
-        url: "/appointment-close/",
-        data : {
-            'appointment':roomHash,
-        },
-        type: 'GET',
-        dataType: 'json',
-        success: function(res) {
-            window.location="/patient-dashboard/";
-        }
-    });
+if (confirm("End Meeting?")) {
+  $.ajax({
+      url: "/appointment-close/",
+      data : {
+          'appointment':roomHash,
+      },
+      type: 'GET',
+      dataType: 'json',
+      success: function(res) {
+          window.location="/patient-dashboard/";
+      }
+  });
 
-  }
+}
 })
 
 function sendSignalingMessage(message) {
-  drone.publish({
-    room: roomName,
-    message
-  });
+drone.publish({
+  room: roomName,
+  message
+});
 }
 
 $(document).on("change",'.pushMed',function(){
-    medicine_name = $(this).val();
-    m = $(this).parent().parent().find(".med-time").children(".M-medicine").is(':checked');
-    l = $(this).parent().parent().find(".med-time").children(".L-medicine").is(':checked');
-    s = $(this).parent().parent().find(".med-time").children(".S-medicine").is(':checked');
-    d = $(this).parent().parent().find(".med-time").children(".D-medicine").is(':checked');
-    aftFood = $(this).parent().parent().find(".aftFood-medicine").is(':checked');
-    befFood = $(this).parent().parent().find(".befFood-medicine").is(':checked');
-    quantity = $(this).parent().parent().find(".quantity-medicine").val();
-    period = $(this).parent().parent().find(".period-medicine").val();
-    remark = $(this).parent().parent().find(".remark-medicine").val();
-    medicine_object = {[medicine_name] : {"m":m,"l":l,"s":s,"d":"d","aftFood":aftFood,"befFood":befFood,"quantity":quantity,"period":period,"remark":remark}}
-    database.ref("meeting/"+roomHash+"/medicines/").update(medicine_object);
+
+  med_row = $(this).closest(".med-row");
+  data_count = med_row.attr("data-count");
+  medicine = med_row.find('.medicine');
+  medicine_name = medicine.val();
+  m = $("#m-"+data_count).is(":checked");
+  l = $("#l-"+data_count).is(":checked");
+  s = $("#s-"+data_count).is(":checked");
+  d = $("#d-"+data_count).is(":checked");
+  aftFood = medicine.parent().parent().find(".aftFood-medicine").is(':checked');
+  befFood = medicine.parent().parent().find(".befFood-medicine").is(':checked');
+  quantity = medicine.parent().parent().find(".quantity-medicine").val();
+  period = medicine.parent().parent().find(".period-medicine").val();
+  remark = medicine.parent().parent().find(".remark-medicine").val();
+  medicine_object = {[data_count] : {"medicine":medicine_name,"m":m,"l":l,"s":s,"d":d,"aftFood":aftFood,"befFood":befFood,"quantity":quantity,"period":period,"remark":remark}}
+  database.ref("meeting/"+roomHash+"/medicines/").update(medicine_object);
 });
 
 if(doctor!="True"){
-    database.ref("meeting/"+roomHash).on("value", function(snapshot) {
-    data = snapshot.val()
-    summary = data["summary"];
-    console.log(data["medicines"]);
-    for(medicine in data["medicines"])
-    {
-        aftFood = data["medicines"][medicine]["aftFood"];
-        befFood = data["medicines"][medicine]["befFood"];
-        d = data["medicines"][medicine]["d"];
-        l = data["medicines"][medicine]["l"];
-        s = data["medicines"][medicine]["s"];
-        m = data["medicines"][medicine]["m"];
-        quantity = data["medicines"][medicine]["quantity"]
-        period = data["medicines"][medicine]["period"]
-        remark = data["medicines"][medicine]["remark"]
-        temp = '<li><strong>'+medicine+'</strong><br><strong>Doses:</strong> ';
-        if (m != false){temp = temp+'Morning'};
-        temp = temp + ', ';
-        if (l != false){temp = temp+'Lunch'};
-        temp = temp + ', ';
-        if (s != false){temp = temp+'Supper'};
-        temp = temp + ', ';
-        if (d != false){temp = temp+'Dinner'};
-        temp = temp + ' <strong>|</strong> ';
-        if (aftFood != false){temp = temp+'After Food'};
-        if (befFood != false){temp = temp+'Before Food'};
-        if (remark != undefined){temp = temp+'<br><strong>Remark: </strong>'+remark};
-        temp = temp + '.</li>';
-        $(".med-repo").append(temp);
-    }
-    console.log(summary);
-    $("#summary-patient").html(summary);
+  database.ref("meeting/"+roomHash).on("value", function(snapshot) {
+  data = snapshot.val()
+  summary = data["summary"];
+  console.log(data["medicines"]);
+  $(".med-repo").empty();
+  for(count in data["medicines"])
+  {
+      medicine_name = data["medicines"][count]["medicine"];
+      aftFood = data["medicines"][count]["aftFood"];
+      befFood = data["medicines"][count]["befFood"];
+      d = data["medicines"][count]["d"];
+      l = data["medicines"][count]["l"];
+      s = data["medicines"][count]["s"];
+      m = data["medicines"][count]["m"];
+      quantity = data["medicines"][count]["quantity"]
+      period = data["medicines"][count]["period"]
+      remark = data["medicines"][count]["remark"]
+      temp = '<li><strong>'+medicine_name+'</strong><br><strong>Doses:</strong> ';
+      if (m != false){temp = temp+'Morning, '};
+      //firebase - m
+      if (l != false){temp = temp+'Noon, '};
+      //firebase - l
+      if (s != false){temp = temp+'Evening, '};
+      //firebase - s
+      if (d != false){temp = temp+'Night'};
+      //firebase - d
+      if (aftFood != false){temp = temp+' <strong></|strong> After Food'};
+      if (befFood != false){temp = temp+' <strong></|strong> Before Food'};
+      temp = temp+"<br><strong>"+quantity+" "+period+"</strong>";
+      if (remark != ""){temp = temp+'<br><strong>Remark: </strong>'+remark};
+      temp = temp + '.</li>';
+
+
+      $(".med-repo").append(temp);
+  }
+  console.log(summary);
+  $("#summary-patient").html(summary);
 
 }, function (errorObject) {
-  console.log("The read failed: " + errorObject.code);
+console.log("The read failed: " + errorObject.code);
 });
 
 }
 
 $("#prescribe-submit").click(function(){
-    console.log('clicked!!!!');
-    summary = $("#summary").val();
-    medicines = [];
-    $('.medicine').each(function() {
-        medicine = $(this).val();
-        m = $(this).parent().parent().find(".med-time").children(".M-medicine").is(':checked');
-        l = $(this).parent().parent().find(".med-time").children(".L-medicine").is(':checked');
-        s = $(this).parent().parent().find(".med-time").children(".S-medicine").is(':checked');
-        d = $(this).parent().parent().find(".med-time").children(".D-medicine").is(':checked');
-        aftFood = $(this).parent().parent().find(".aftFood-medicine").is(':checked');
-        befFood = $(this).parent().parent().find(".befFood-medicine").is(':checked');
-        quantity = $(this).parent().parent().find(".quantity-medicine").val();
-        period = $(this).parent().parent().find(".period-medicine").val();
-        remark = $(this).parent().parent().find(".remark-medicine").val();
+  console.log('clicked!!!!');
+  summary = $("#summary").val();
+  medicines = [];
+  $('.medicine').each(function() {
+      medicine = $(this).val();
+      m = $(this).parent().parent().find(".med-time").children(".M-medicine").is(':checked');
+      l = $(this).parent().parent().find(".med-time").children(".L-medicine").is(':checked');
+      s = $(this).parent().parent().find(".med-time").children(".S-medicine").is(':checked');
+      d = $(this).parent().parent().find(".med-time").children(".D-medicine").is(':checked');
+      aftFood = $(this).parent().parent().find(".aftFood-medicine").is(':checked');
+      befFood = $(this).parent().parent().find(".befFood-medicine").is(':checked');
+      quantity = $(this).parent().parent().find(".quantity-medicine").val();
+      period = $(this).parent().parent().find(".period-medicine").val();
+      remark = $(this).parent().parent().find(".remark-medicine").val();
 
-        medicines.push({"medicine":medicine,"m":m,"l":l,"s":s,"d":d,"aftFood":aftFood,"befFood":befFood,"quantity":quantity,"period":period,"remark":remark});
-        });
+      medicines.push({"medicine":medicine,"m":m,"l":l,"s":s,"d":d,"aftFood":aftFood,"befFood":befFood,"quantity":quantity,"period":period,"remark":remark});
+      });
 
-        $.ajax({
-        url: "/prescription-submit/",
-        data : {
-            'summary':summary,
-            'medicines':JSON.stringify(medicines),
-            'roomhash':roomHash,
-        },
-        type: 'GET',
-        dataType: 'json',
-        success: function(res) {
-            alert("Submitted!!");
-        }
-    });
+      $.ajax({
+      url: "/prescription-submit/",
+      data : {
+          'summary':summary,
+          'medicines':JSON.stringify(medicines),
+          'roomhash':roomHash,
+      },
+      type: 'GET',
+      dataType: 'json',
+      success: function(res) {
+          alert("Submitted!!");
+      }
+  });
 
 });
+
+
